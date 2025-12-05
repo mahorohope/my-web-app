b521fe587a1b39ef283039431f618eec3fa8c160
MAHORO Hope <mahorohope56@gmail.com>
2025-12-05 13:41:32 -0800

    Add about route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   if (req.url === '/about') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('About Page\n');
+    res.end('About Page.\n');
   } else {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
